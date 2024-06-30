f8b2adb8148310d8c55a4f6b0bee5304e06bc7a0
k-nomoto1728 <nomotokeiichiro@icloud.com>
2024-07-01 00:03:33 +0900

    Add files via upload

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
     var $button = document.querySelector('.toggle-menu-button');
-    var $menu = document.querySelector('.header-site-menu');
+    var $menu = document.querySelector('.header-menu');
     $button.addEventListener('click', function () {
         if ($menu.classList.contains('is-show')) {
             $menu.classList.remove('is-show');
